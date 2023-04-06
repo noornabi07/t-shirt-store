653f753c391831953958ea1cb18bb63b3a21e53a
noornabi07 <noornabiprogram07@gmail.com>
2023-04-06 23:53:11 +0600

    t-shirt online store will start first setup done

---
+++ src/Components/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>This is order page here...</h2>
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import MainLayout from './Components/Layout/MainLayout';
+import Orders from './Components/Orders/Orders';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('data.json')
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
